c75423761e85aeb5ce7c63473407fe40abf75606
Shinjihwan <jihwan9675@gmail.com>
2021-04-15 04:45:37 +0900

    templates 추가 Login, Register Form 추가 Fcuser 모델 추가 app.py에 라우팅 메소드들 추가

--- Project/models.py
+++ Project/models.py
@@ -6,6 +6,7 @@ class Todo(db.Model):
     __tablename__ = 'todo'
 
     id = db.Column(db.Integer, primary_key=True)
+    fcuser_id = db.Column(db.Integer, db.ForeignKey('fcuser.id'), nullable = False)
     title = db.Column(db.String(256))
     tstamp = db.Column(db.DateTime, server_default = db.func.now())
 
@@ -16,3 +17,11 @@ class Todo(db.Model):
             'title':self.title,
             'tstamp':self.tstamp
         }
+
+class Fcuser(db.Model):
+    __tablename__ = 'fcuser'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+    todos = db.relationship('Todo', backref='fcuser', lazy=True)
